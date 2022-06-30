8da8ccb47531b392c35589557e9f11db067e507a
rogerio <rogeriofilho2@gmail.com>
2022-06-30 19:03:11 -0300

    versao completa

--- convolution.cpp
+++ convolution.cpp
@@ -34,12 +34,26 @@ void init_images(){
 }
 
 void convolution_version_1(){
-    for(int linha = 0; linha < IMAGE_SIZE; linha++){
-        for(int coluna = 0; coluna < IMAGE_SIZE; coluna++){
+    for(int coluna = 0; coluna < IMAGE_SIZE; coluna++){
+       for(int linha = 0; linha < IMAGE_SIZE; linha++){
+            float sum = 0;
+            for(int n = coluna - KERNEL_AMPLITUDE; n <= coluna + KERNEL_AMPLITUDE; n++){
+                for(int m = linha - KERNEL_AMPLITUDE; m <= linha + KERNEL_AMPLITUDE; m++){
+                    if (m >= 0 && m < IMAGE_SIZE && n >= 0 && n < IMAGE_SIZE)
+                        sum += (image[m][n] / KERNEL_SIZE*KERNEL_SIZE);    
+                }
+            }
+            new_image[linha][coluna] = sum;
+        }
+    }
+}
+
+void convolution_version_optimized(){
+    for(int linha = KERNEL_AMPLITUDE; linha < IMAGE_SIZE - KERNEL_AMPLITUDE; linha++){
+        for(int coluna = KERNEL_AMPLITUDE; coluna < IMAGE_SIZE - KERNEL_AMPLITUDE; coluna++){
             float sum = 0;
             for(int m = linha - KERNEL_AMPLITUDE; m <= linha + KERNEL_AMPLITUDE; m++){
                 for(int n = coluna - KERNEL_AMPLITUDE; n <= coluna + KERNEL_AMPLITUDE; n++){
-                    if (m >= 0 && m < IMAGE_SIZE && n >= 0 && n < IMAGE_SIZE)
                         sum += (image[m][n] / KERNEL_SIZE*KERNEL_SIZE);    
                 }
             }
@@ -76,19 +90,35 @@ int main(){
 
     auto end = chrono::steady_clock::now();
 
-    int elapsed_time = chrono::duration_cast<chrono::seconds>(end - start).count();
+    auto duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+    float elapsed_time = float(duration)/1000000;
 
     cout << "Elapsed time in convolution version 1: " << elapsed_time << "s.\n";
 
+    start = chrono::steady_clock::now();
+
+    convolution_version_optimized();
+
+    end = chrono::steady_clock::now();
+
+    duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+    elapsed_time = float(duration)/1000000;
+
+    cout << "Elapsed time in convolution version optimized: " << elapsed_time << "s.\n";
+
+    /*
+    /////////////////////////////OPEN MP//////////////////////////////////////////
     start = chrono::steady_clock::now();
 
     convolution_version_openmp();
 
     end = chrono::steady_clock::now();
 
-    elapsed_time = chrono::duration_cast<chrono::seconds>(end - start).count();
+    duration = chrono::duration_cast<chrono::microseconds>(end - start).count();
+    elapsed_time = float(duration)/1000000;
 
-    cout << "Elapsed time in convolution version openmp: " << elapsed_time << "s.";
-    
+    cout << "Elapsed time in convolution version openmp: " << elapsed_time << "s.\n";
+    /////////////////////////////FINISHED OPEN MP//////////////////////////////////////////
+    */
     return 0;
 }
